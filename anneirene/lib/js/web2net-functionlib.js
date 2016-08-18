8fc9e7a267876e0d41832e788bba71e70822c90f
anneirene <anne@web2net.no>
2016-08-18 12:36:09 +0200

    Kopiert inn original

---
+++ anneirene/lib/js/web2net-functionlib.js
@@ -0,0 +1,116 @@
+
+function adjustStyle()
+{
+    setCSSInlineStyle();
+    if (StyleMode == true)
+    {
+	    if ($("#div-window-size").length == 0)
+	    {    
+	        $("body").append("<div id=\"div-window-size\" style=\"position: fixed; z-index: 499999; top: 0; left: 0; width: 100%; height: 20px; text-align: center;\"></div>");
+		}
+    }
+    var _WindowWidth = parseInt($(window).width());
+    if (_WindowWidth >= 1200)
+    {
+        $("#size-stylesheet").attr("href", "lib/css/lg.css");
+        $("#div-window-size").css({ "background-color": "#FF0000" }).text("lg.css");
+    }
+    else if (_WindowWidth >= 992 && _WindowWidth < 1200)
+    {
+        $("#size-stylesheet").attr("href", "lib/css/md.css");
+        $("#div-window-size").css({ "background-color": "#428bca" }).text("md.css");
+    }
+    else if (_WindowWidth >= 768 && _WindowWidth < 991)
+    {
+        $("#size-stylesheet").attr("href", "lib/css/sm.css");
+        $("#div-window-size").css({ "background-color": "#f0ad4e" }).text("sm.css");
+    }
+    else
+    {
+        $("#size-stylesheet").attr("href", "lib/css/xs.css");
+        $("#div-window-size").css({ "background-color": "#5cb85c" }).text("xs.css");
+    }
+}
+
+function setCSSInlineStyle()
+{
+    var _WindowWidth = parseInt($(window).width());
+    if (_WindowWidth >= 1200)
+    {
+        _WindowWidth = "lg";
+    }
+    else if (_WindowWidth >= 992 && _WindowWidth < 1200)
+    {
+        _WindowWidth = "md";
+    }
+    else if (_WindowWidth >= 768 && _WindowWidth < 991)
+    {
+        _WindowWidth = "sm";
+    }
+    else
+    {
+        _WindowWidth = "xs";
+    }
+    if (_WindowWidth != "")
+    {
+        var ClassName = "";
+        var ClassItem = "";
+        var ObjSetStyle = {};
+        $("[class*='web2net-']").each(function()
+        {
+            var _Self = $(this);
+            ClassName = "";
+            ClassItem = "";
+            ObjSetStyle = {};
+            ClassName = (this.className.match(/[\w-]*web2net-[\w-#%]*/g));
+            if (ClassName.length > 0)
+            {
+                $.each(ClassName, function(i, SelectedElement)
+                {
+                    ClassItem = SelectedElement.split("-");
+                    if (ClassItem.length > 3)
+                    {
+                        if (ClassItem[1] == _WindowWidth || ClassItem[1] == "all")
+                        {
+                            if (ObjSetStyle.hasOwnProperty(ClassItem[2]) == false)
+                            {
+                                if (ClassItem.length == 4)
+                                {
+                                    ObjSetStyle[ClassItem[2]] = ClassItem[3];
+                                }
+                                else if (ClassItem.length == 5)
+                                {
+                                    ObjSetStyle[ClassItem[2]] = ClassItem[3] + " " + ClassItem[4];
+                                }
+                                else if (ClassItem.length == 6)
+                                {
+                                    ObjSetStyle[ClassItem[2]] = ClassItem[3] + " " + ClassItem[4] + " " + ClassItem[5];
+                                }
+                            }
+                            else
+                            {
+                                if (ClassItem[1] == _WindowWidth)
+                                {
+                                    if (ClassItem.length == 4)
+                                    {
+                                        ObjSetStyle[ClassItem[2]] = ClassItem[3];
+                                    }
+                                    else if (ClassItem.length == 5)
+                                    {
+                                        ObjSetStyle[ClassItem[2]] = ClassItem[3] + " " + ClassItem[4];
+                                    }
+                                    else if (ClassItem.length == 6)
+                                    {
+                                        ObjSetStyle[ClassItem[2]] = ClassItem[3] + " " + ClassItem[4] + " " + ClassItem[5];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+            _Self.removeAttr("style");
+            _Self.css(ObjSetStyle)
+        });
+    }
+}
